52a64706f73ed445985070a9bbd783d47736dab1
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-07-24 21:51:19 +0530

    mongodb connection

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 // Express Library
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+mongoose.connect('mongodb://localhost/urlShortner',{
+    useNewUrlParser: true, useUnifiedTopology: true,
+})
 // set up View Engine
 app.set('view engine','ejs')
 
 // Defining route with request and response variables
 app.get('/',(req,res) => {
     res.render('index')                  // rendering index.ejs
+})
+
+// Creating POST end-points 
+
+app.post('/shortUrls',(req,res) => {
+
 })
 app.listen(process.env.PORT || 5000);
